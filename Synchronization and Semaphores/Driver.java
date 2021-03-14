ba58cab3c123d4ce5270dfd4716c7b3b0d9a6016
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-03-15 00:22:55 +0400

    Add Synchronization

---
+++ Synchronization and Semaphores/Driver.java
@@ -0,0 +1,27 @@
+
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    	Network objNetwork = new Network( );                        /* Activate the network */
+        objNetwork.start();
+
+        Server objServer1 = new Server("Thread1");
+        objServer1.start();
+        Server objServer2 = new Server("Thread2");
+        objServer2.start();
+
+        Client objClient1 = new Client("sending");          /* Start the sending client thread */
+        objClient1.start();
+        Client objClient2 = new Client("receiving");        /* Start the receiving client thread */
+        objClient2.start();
+        
+      /*..............................................................................................................................................................*/
+       
+    }
+    
+ }
