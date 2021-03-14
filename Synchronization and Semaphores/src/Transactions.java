eedc3b3d8b25dca3cd36a96e7dd3cdda48585d06
Vaansh Lakhwara <vaanshlakhwara@gmail.com>
2021-03-15 00:33:52 +0400

    Clean Code Indentation

--- Synchronization and Semaphores/src/Transactions.java
+++ Synchronization and Semaphores/src/Transactions.java
@@ -1,14 +1,3 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/** Transactions class
- *
- * @author Kerly Titus
- */
 public class Transactions {
     
     private String accountNumber;       /* Account number */
